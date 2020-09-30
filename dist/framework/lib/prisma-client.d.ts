5486581d66e563c1e77fe81224c66593aa09f504
David Lee <davidhollandlee@gmail.com>
2020-09-29 21:40:45 -0400

    ran build to add /dist directory

---
+++ dist/framework/lib/prisma-client.d.ts
@@ -0,0 +1,5 @@
+import { RuntimeLens } from 'nexus/plugin';
+import { Settings } from '../settings';
+export declare function getPrismaClientInstance(clientOrOptions: Settings['client'], log: RuntimeLens['log']): any;
+export declare function getPrismaClientDir(): string;
+//# sourceMappingURL=prisma-client.d.ts.map
